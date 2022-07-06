f68fe7cdbb0c4a5acfa550216d2a32656de3fea9
mapp190413 <mapp190413@sri.ad>
2022-07-06 16:49:46 -0500

    Se suben cambios para envio documento

--- src/AppUpload.js
+++ src/AppUpload.js
@@ -1,97 +1,122 @@
 import axios from 'axios';
- 
-import React,{Component} from 'react';
- 
+
+import React, { Component } from 'react';
+
 class App extends Component {
-  
-    state = {
- 
-      // Initially, no file is selected
-      selectedFile: null
-    };
-    
-    // On file select (from the pop up)
-    onFileChange = event => {
-    
-      // Update the state
-      this.setState({ selectedFile: event.target.files[0] });
-    
-    };
-    
-    // On file upload (click the upload button)
-    onFileUpload = () => {
-    
+
+
+  constructor(props) {
+    super(props);
+    this.onFileChange = this.onFileChange.bind(this);
+    this.onFileUpload = this.onFileUpload.bind(this);
+    this.state = {
+      selectedFiles: ''
+    }
+}
+
+  // On file select (from the pop up)
+  onFileChange(e) {
+
+    // Update the state
+    this.setState({ selectedFiles: e.target.files });
+
+  };
+
+  // On file upload (click the upload button)
+  onFileUpload (e) {
+    e.preventDefault();
+    for (const key of Object.keys(this.state.selectedFiles)) {
       // Create an object of formData
+      console.log(key);
+      var selectedFile = this.state.selectedFiles[key];
       const formData = new FormData();
-    
+
       // Update the formData object
       formData.append(
-        "myFile",
-        this.state.selectedFile,
-        this.state.selectedFile.name
+        "myFile"+key,
+        selectedFile,
+        selectedFile.name
       );
-    
+
       // Details of the uploaded file
-      console.log(this.state.selectedFile);
-    
+      console.log(selectedFile);
+
       // Request made to the backend api
       // Send formData object
-      axios.post("api/uploadfile", formData);
-    };
-    
-    // File content to be displayed after
-    // file upload is complete
-    fileData = () => {
-    
-      if (this.state.selectedFile) {
-         
-        return (
-          <div>
+      axios.post("api/uploadfile", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    });
+
+    }
+
+  };
+
+  // File content to be displayed after
+  // file upload is complete
+  fileData() {    
+    var dom_content = [];
+    for (const key of Object.keys(this.state.selectedFiles)) {
+      console.log(key);
+      var selectedFile = this.state.selectedFiles[key];
+      if (selectedFile) {
+
+        dom_content.push(
+          <div id={key}>
             <h2>File Details:</h2>
-             
-<p>File Name: {this.state.selectedFile.name}</p>
- 
-             
-<p>File Type: {this.state.selectedFile.type}</p>
- 
-             
-<p>
+
+            <p>File Name: {selectedFile.name}</p>
+
+
+            <p>File Type: {selectedFile.type}</p>
+
+
+            <p>
               Last Modified:{" "}
-              {this.state.selectedFile.lastModifiedDate.toDateString()}
+              {selectedFile.lastModifiedDate.toDateString()}
             </p>
- 
+
           </div>
         );
       } else {
-        return (
+        dom_content.push (
           <div>
             <br />
             <h4>Choose before Pressing the Upload button</h4>
           </div>
         );
       }
-    };
-    
-    render() {
-    
-      return (
-        <div>
-            <h1>
-              GeeksforGeeks
-            </h1>
-            <h3>
-              File Upload using React!
-            </h3>
-            <div>
-                <input type="file" onChange={this.onFileChange} />
-                <button onClick={this.onFileUpload}>
-                  Upload!
-                </button>
-            </div>
-          {this.fileData()}
-        </div>
-      );
     }
+    return (
+      <div> 
+          {dom_content}
+      </div>
+  );
+
+
+  };
+
+  render() {
+
+    return (
+      <div>
+        <h1>
+          GeeksforGeeks
+        </h1>
+        <h3>
+          File Upload using React!
+        </h3>
+        <div>
+          <input type="file" onChange={this.onFileChange} multiple accept="text/xml" />
+          <button onClick={this.onFileUpload}>
+            Upload!
+          </button>
+        </div>
+        {this.fileData()}
+      </div>
+    );
   }
- 
+}
+
 export default App;
